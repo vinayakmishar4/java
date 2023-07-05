2f037af8913a1ba923b207047ed1917c1686999b
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2023-07-05 10:40:08 +0530

    Add files via upload

---
+++ Hackerrank/BigInteger.java
@@ -0,0 +1,15 @@
+package Hackerrank;
+import java.io.*;
+import java.util.*;
+
+public class BigInteger {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println(a+b);
+        System.out.println(a*b);
+    }
+}
