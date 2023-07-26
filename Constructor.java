001ee9558f0a21d6ff6e1594fcc8dbe5c9805416
Vinayak Mishra <88668137+vinayakmishar4@users.noreply.github.com>
2023-07-26 22:58:19 +0530

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,25 @@
+class Constructor
+{
+    //default constructor
+    // int i;
+    // String s;
+    // user defined constructor
+    Constructor()
+    {
+        System.out.println("New Constructor");
+    }
+    // parametric constructor 
+    // Constructor(String name)
+    // {
+    // }
+   public static void main(String[] args) 
+   {
+        // user defined constructor
+        Constructor c=new Constructor();
+        // parametric constructor      
+       //Constructor s=new Constructor("Vinayak");
+      // default constructor
+      //System.out.println(s.i + " " + s.s);
+
+   }
+}
